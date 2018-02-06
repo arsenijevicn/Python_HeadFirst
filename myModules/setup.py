88ae66cb1295a798fb79ef3c8a0c86c9f6452d51
Nebojsa Arsenijevic <arsenijevicn@gmail.com>
2018-02-06 15:20:46 +0100

    Dodato sa posla 06.02.2018 strana 182 Problem sa instalacijom vsearch

---
+++ myModules/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="vsearch_n",
+    version="1.0",
+    description="The Head First python search tools",
+    author="HF Python 2e",
+    author_email="hfpy2e@gmail.com",
+    url="headfirstlabs.com",
+    py_modules=["vsearch_n"],
+)
+
